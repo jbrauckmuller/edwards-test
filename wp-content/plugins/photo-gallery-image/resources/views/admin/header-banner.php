5f08e6b8bdba4777fba35e5043327984a210f97e
jbrauckmuller <jamiebrauckmuller@gmail.com>
2018-09-19 16:54:26 -0400

    picture gallery - execs added

---
+++ wp-content/plugins/photo-gallery-image/resources/views/admin/header-banner.php
@@ -0,0 +1,21 @@
+<div class="wrap gdgallery_list_container ">
+    <div class="gdgallery_header">
+        <i class="gdicon gdicon-logo"></i>
+        <span><?php _e('GrandWP Gallery', GDGALLERY_TEXT_DOMAIN); ?></span>
+
+        <ul>
+            <li>
+                <a href="https://demo.grandwp.com/wordpress-photo-gallery-justified/" target="_blank"><?php _e('Demo', GDGALLERY_TEXT_DOMAIN); ?></a>
+            </li>
+            <li>
+                <a href="https://wordpress.org/support/plugin/photo-gallery-image" target="_blank"><?php _e('Support', GDGALLERY_TEXT_DOMAIN); ?></a>
+            </li>
+            <li>
+                <a href="https://grandwp.com/wordpress-photo-gallery" target="_blank"><?php _e('Go Pro', GDGALLERY_TEXT_DOMAIN); ?></a>
+            </li>
+            <li>
+                <a href="https://grandwp.com/grandwp-photo-gallery-user-manual" target="_blank"><?php _e('Help', GDGALLERY_TEXT_DOMAIN); ?></a>
+            </li>
+        </ul>
+    </div>
+</div>
